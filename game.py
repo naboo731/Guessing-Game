75d1cc9be2dd6d7d544e7f516bcaf05fccb4a034
Nichole Boothman <nbooth731@gmail.com>
2022-01-11 12:52:41 -0500

    added int checker

--- game.py
+++ game.py
@@ -5,4 +5,17 @@ from random import randint
 print('Hello and welcome to the Number Guessing Game!')
 print('What is your name?')
 
-name = input('(type name your name in quotes)')
+name = input('(type your name)')
+
+print(f'{name}, Im thinking of a number 1 to 100')
+print('Can you guess the number?')
+
+while True:
+    guess = input('type your guess')
+
+    try:
+        guess = int(guess)
+    except ValueError:
+        print(f'"{guess}" is not a valid number. Please try again')
+        continue
+    if
