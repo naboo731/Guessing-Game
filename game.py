3bde529489cd72bfc8d5ba989877383133a3f3a9
Nichole Boothman <nbooth731@gmail.com>
2022-01-11 13:02:24 -0500

    complete

--- game.py
+++ game.py
@@ -2,6 +2,9 @@
 
 from random import randint
 
+attempts = 0
+number = randint(1, 100)
+
 print('Hello and welcome to the Number Guessing Game!')
 print('What is your name?')
 
@@ -18,4 +21,17 @@ while True:
     except ValueError:
         print(f'"{guess}" is not a valid number. Please try again')
         continue
-    if
+    if guess < 1 and guess > 100:
+        print('Please enter a number between 1 and 100')
+        continue
+
+    attempts += 1
+
+    if guess < number:
+        print('Your guess is too low. Try again.')
+        continue
+    elif guess > number:
+        print('Your guess is too high. Try again.')
+    else:
+        print(f'{guess} is right! Well done, {name}')
+        print(f'You were able to guess the number in {attempts} attempts!')
